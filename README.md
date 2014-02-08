d7f43e2dc93108c03c7cce20dc30547a0158d755
James Matthews <jfmatt8067@gmail.com>
2014-02-08 00:30:46 -0500

    push to npm

---
+++ README.md
@@ -0,0 +1,45 @@
+# gulp-requirejs-replace-script
+[![NPM version][npm-image]][npm-url]  [![Coverage Status](coveralls-image)](coveralls-url) [![Dependency Status][depstat-image]][depstat-url]
+
+> Plugin for [gulp](https://github.com/wearefractal/gulp) to replace RequireJS includes with direct <script> tags for AlmondJS builds, like the [replaceRequireScript](https://github.com/asciidisco/grunt-requirejs/blob/master/docs/almondIntegration.md) functionality from [grunt-requirejs](https://github.com/asciidisco/grunt-requirejs).
+
+## Usage
+
+First, install `gulp-requirejs-replace-script` as a development dependency:
+
+```shell
+npm install --save-dev gulp-requirejs-replace-script
+```
+
+Then, add it to your `gulpfile.js`:
+
+```javascript
+var rjsReplace = require("gulp-requirejs-replace-script");
+
+gulp.src("./client/src/index.html")
+	.pipe(rjsReplace([
+		'js/main'
+	]))
+	.pipe(gulp.dest("./client/dist"));
+```
+
+## API
+
+### requirejs-replace-script(modules)
+
+#### modules
+Type: `Array`  
+
+An array of module names which should be included directly rather than via RequireJS's `data-main` attribute.
+
+If not provided, all Require tags will be replaced.
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
+
+[npm-url]: https://npmjs.org/package/gulp-requirejs-replace-script
+[npm-image]: https://badge.fury.io/js/gulp-requirejs-replace-script.png
+
+[depstat-url]: https://david-dm.org/jfmatt/gulp-requirejs-replace-script
+[depstat-image]: https://david-dm.org/jfmatt/gulp-requirejs-replace-script.png
---
+++ index.js
@@ -0,0 +1,76 @@
+var fs = require('fs'),
+    
+    through = require("through2"),
+    gutil = require("gulp-util"),
+    jsdom = require("jsdom")
+
+module.exports = function (modules) {
+
+	function replaceModuleP (name) {
+		return !modules || (modules.indexOf(name) != -1) 
+	}
+
+	function doReplace (html, next) {
+		jsdom.env(
+			html,
+			{
+				FetchExternalResources: false,
+				ProcessExternalResources: false
+			},
+			function (err, window) {
+				var scripts = window.document.querySelectorAll('script[data-main]')
+				;[].slice.call(scripts).forEach(function (s) {
+					var module = s.getAttribute('data-main')
+					
+					if (replaceModuleP(module)) {
+						s.src = module + '.js'
+						s.removeAttribute('data-main')
+					}
+
+				})
+
+				var ret = window.document.innerHTML
+				window.close()
+				return next(ret)
+			}
+		)
+	}
+		
+	
+	function replaceScripts(file, enc, next) {
+
+		// Do nothing if no contents
+		if (file.isNull()) {
+			this.push(file)
+			return next()
+		}
+
+		
+		if (file.isStream()) {
+			var html = ""
+			file.contents.on('data', function (d) {
+				html += d
+			})
+			return file.contents.on('end', function () {
+				doReplace(html, function (res) {
+					file.contents = through()
+					this.push(file)
+					file.contents.write(res)
+					file.contents.end()
+					return next()
+				}.bind(this))
+			}.bind(this))
+		}
+
+		if (file.isBuffer()) {
+			return doReplace(file.contents.toString(), function (res) {
+				file.contents = new Buffer(res)
+				this.push(file)
+				return next()
+			}.bind(this))
+		}
+		return next()
+	}
+
+	return through.obj(replaceScripts)
+};
